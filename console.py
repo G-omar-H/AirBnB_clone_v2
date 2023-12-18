100bafcf7bf79b34c599c2d9bdb9b70d244570c4
G-omar-H <omarghazi12@gmail.com>
2023-12-18 18:10:11 +0100

    2. Console improvements

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,13 +115,32 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        cls = args.partition(" ")[0]
+        p_list = args.partition(" ")[2].split(" ")
+        paras = [p for p in p_list if p != ""]
+        if not cls:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif cls not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[cls]()
+        for ele in paras:
+            key = ele.partition("=")[0]
+            value = ele.partition("=")[2]
+            if hasattr(HBNBCommand.classes[cls], key):
+                if key in HBNBCommand.types.keys():
+                    value = HBNBCommand.types[key](value)
+                if type(value) is str and ("_" in value or "\"" in value):
+                    while "_" in value:
+                        value = value.partition("_")[0] + " "
+                        + value.partition("_")[2]
+                    if value[0] in ['"', "'"] and value[0][-1] in ['"', "'"]:
+                        value = value[1:-1]
+                    if '"' in value:
+                        value = value.partition("\"")[0] +\
+                            '\\' + '\"' + value.partition("\"")[2]
+                setattr(new_instance, key, value)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -187,7 +206,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -272,7 +291,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +299,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +338,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
