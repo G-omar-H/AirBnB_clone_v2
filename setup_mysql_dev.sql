e9263a758fb921b4d4615262e9505cc08cc927bc
G-omar-H <omarghazi12@gmail.com>
2023-12-18 21:08:28 +0100

    3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,6 +6,6 @@
 -- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED  BY 'hbnb_dev_pwd';
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-GRANT ALL PRIVILEGES  ON `hbnb_dev_db`.*  TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES  ON `hbnb_dev_db`.*  TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON  `performance_schema`.* TO 'htbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
