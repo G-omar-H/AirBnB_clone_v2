943b3da94a94e2acb9d4a7a63b35c13c6de46160
G-omar-H <omarghazi12@gmail.com>
2023-12-20 18:20:46 +0100

    8. DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -7,8 +7,13 @@ import models
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """The city class, contains state ID and name"""
+
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship('State', back_populates='cities')
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
+
+    places = relationship(
+        "Place", back_populates="cities", cascade="all, delete-orphan"
+    )
--- models/state.py
+++ models/state.py
@@ -6,8 +6,10 @@ from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """State class"""
+
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state',
+    cities = relationship("City",
+                          back_populates="state",
                           cascade="all, delete-orphan")
