ab5620a4c25cbf77bcd4fc56cb417586f635acb3
G-omar-H <omarghazi12@gmail.com>
2024-01-11 17:40:10 +0100

    1. Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    try:
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(current_time)
+        local("mkdir -p versions")
+        local("tar -czvf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
