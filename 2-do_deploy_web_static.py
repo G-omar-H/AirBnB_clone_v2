1e76b169a2a5a825d04482a1f78cab95515f19fe
G-omar-H <omarghazi12@gmail.com>
2024-01-11 22:37:57 +0100

    2. Add a custom HTTP header with Puppet

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,31 +28,26 @@ def do_pack():
 
 @task
 def do_deploy(archive_path):
+    """ method doc
+        fab -f 2-do_deploy_web_static.py do_deploy:
+        archive_path=versions/web_static_20231004201306.tgz
+        -i ~/.ssh/id_rsa -u ubuntu
     """
-    distributes an archive to your web servers
-    """
-    if not os.path.exists(archive_path):
-        return False
-
     try:
-        put(archive_path, '/tmp/')
-
-        filename = os.path.basename(archive_path)
-        foldername = filename.split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(foldername))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(filename, foldername))
-  
-        run('rm /tmp/{}'.format(filename))
-        run('mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/'.format(foldername, foldername))
-        run('rm -rf /data/web_static/releases/\
-            {}/web_static/*'.format(foldername))
-        run('rm -rf /data/web_static/current')
-
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(foldername))
-
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
         print("New version deployed!")
         return True
     except Exception:
