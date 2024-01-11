b826426a29f8c9bf2490ee388c093a79639d84f0
G-omar-H <omarghazi12@gmail.com>
2024-01-11 21:54:03 +0100

    3. Full deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,18 +1,24 @@
+usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers
+"""
+from fabric.api import *
+import os
+
 #!/usr/bin/python3
 """
 Fabric script that distributes an archive to your web servers
 """
 
-from fabric.api import env, put, run
-import os
-
-env.user = 'ubuntu'
 env.hosts = ['100.25.15.47', '52.91.121.153']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
 
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to your web servers
+    distributes an archive to your web servers
     """
     if not os.path.exists(archive_path):
         return False
@@ -21,17 +27,18 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
 
         filename = os.path.basename(archive_path)
-        folder_name = '/data/web_static/releases/' +\
-            os.path.splitext(filename)[0]
-        run('mkdir -p {}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, folder_name))
-
+        foldername = filename.split('.')[0]
+        run('mkdir -p /data/web_static/releases/{}'.format(foldername))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(filename, foldername))
         run('rm /tmp/{}'.format(filename))
 
         run('rm -rf /data/web_static/current')
 
-        run('ln -s {} /data/web_static/current'.format(folder_name))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(foldername))
 
         return True
     except Exception:
         return False
+
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,11 +3,51 @@
 Fabric script that creates and distributes an archive to your web servers,
 """
 
-from fabric.api import env
+from fabric.api import env, put, run, local
+from datetime import datetime
+import os
 
-
-env.user = 'ubuntu'
 env.hosts = ['100.25.15.47', '52.91.121.153']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_deploy(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, '/tmp/')
+
+        filename = os.path.basename(archive_path)
+        foldername = filename.split('.')[0]
+        run('mkdir -p /data/web_static/releases/{}'.format(foldername))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(filename, foldername))
+        run('rm /tmp/{}'.format(filename))
+
+        run('rm -rf /data/web_static/current')
+
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(foldername))
+
+        return True
+    except Exception:
+        return False
+
+
+def do_pack():
+    """
+    pack .tgz archive from web_static folder
+    """
+    try:
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(current_time)
+        local("mkdir -p versions")
+        local("tar -czvf {} web_static".format(archive_path))
+        return archive_path
+    except Exception:
+        return None
 
 
 def deploy():
