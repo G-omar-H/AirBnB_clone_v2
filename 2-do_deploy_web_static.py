6997c200d06fe1befa53f29eaeec03e59d78350f
G-omar-H <omarghazi12@gmail.com>
2024-01-11 22:33:56 +0100

    2. Add a custom HTTP header with Puppet

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -42,9 +42,10 @@ def do_deploy(archive_path):
         run('mkdir -p /data/web_static/releases/{}'.format(foldername))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
             .format(filename, foldername))
+        
         run('rm /tmp/{}'.format(filename))
-
-        run('rm -rf /data/web_static/releases/{}/web_static/*'.format(foldername))
+        run('rm -rf /data/web_static/releases/\
+            {}/web_static/*'.format(foldername))
         run('rm -rf /data/web_static/current')
 
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -43,14 +43,11 @@ def do_deploy(archive_path):
         run('mkdir -p /data/web_static/releases/{}'.format(foldername))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
             .format(filename, foldername))
+        
         run('rm /tmp/{}'.format(filename))
-
+        run('rm -rf /data/web_static/releases/{}/\
+            web_static/*'.format(foldername))
         run('rm -rf /data/web_static/current')
-
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'
-            .format(foldername, foldername))
-
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
             .format(foldername))
 
