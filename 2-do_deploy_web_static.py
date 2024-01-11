8678d8a754cacd990ca7cac2d1fd9f6fbc285541
G-omar-H <omarghazi12@gmail.com>
2024-01-11 21:15:51 +0100

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" Fabric script that distributes an archive to your web servers,"""
+"""
+Fabric script that distributes an archive to your web servers
+"""
 from fabric.api import env, put, run
 import os
 
 env.hosts = ['100.25.15.47', ' 	52.91.121.153']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
