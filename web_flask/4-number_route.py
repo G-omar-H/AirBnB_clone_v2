6cb6c72629f9f2bc6a49e88fd34b379ff078c4de
G-omar-H <omarghazi12@gmail.com>
2024-01-26 09:55:47 +0100

    4. Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -53,10 +53,9 @@ def python_text(text='is cool'):
     return f"Python {res}"
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
-    if n.isdigit():
-        return f'{n} is a number'
+    return f'{n} is a number'
 
 
 if __name__ == '__main__':
